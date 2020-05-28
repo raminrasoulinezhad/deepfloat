219bc4ced84e1500027a93e0ebb55d237277cb49
Seyedramin Rasoulinezhad <ramin_r348@yahoo.com>
2020-05-28 14:03:50 +1000

    update

--- rtl/tests/runs_asic.sh
+++ rtl/tests/runs_asic.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/sh
 
+mkdir -p ./paper_asic_auto
+
 NUM_CORES=16
 
 parallel --bar --gnu -j${NUM_CORES} --header : \
-    'bash ./fff_asic_exp.sh paper_auto {a_e} {a_f} {b_e} {b_f} {acc_e} {acc_f} {period}' \
+    'bash ./fff_asic_exp.sh paper_asic_auto {a_e} {a_f} {b_e} {b_f} {acc_e} {acc_f} {period}' \
     ::: a_e 0 1 2 3 4 5 \
-    ::: a_f 0 1 2 3 4 5 6 7 \
+    ::: a_f 0 1 2 3 4 5 \
     ::: b_e 0 1 2 3 4 5 \
-    ::: b_f 0 1 2 3 4 5 6 7 \
+    ::: b_f 0 1 2 3 4 5 \
     ::: acc_e 0 1 2 3 4 5 \
-    ::: acc_f 0 1 2 3 4 5 6 7 \
-    ::: period 100 1333 2000
+    ::: acc_f 0 1 2 3 4 5 \
+    ::: period 1000 1333 2000
 
