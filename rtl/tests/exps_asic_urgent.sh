ff43e32106b9977e9253a7c6b89e7d78cde42d37
Seyedramin Rasoulinezhad <ramin_r348@yahoo.com>
2020-05-31 22:12:08 +1000

    urgent results

---
+++ rtl/tests/exps_asic_urgent.sh
@@ -0,0 +1,18 @@
+#############################################################################
+mkdir -p paper_urgent
+
+make ffk_POC_asic  addr=./paper_urgent/ffk_POC_asic_A_5_2_B_4_3_ACC_56_MHz750    a_e=5  a_f=2  b_e=4  b_f=3  acc_d=56  period=1333
+make ffk_POC_asic  addr=./paper_urgent/ffk_POC_asic_A_5_2_B_5_2_ACC_71_MHz750    a_e=5  a_f=2  b_e=5  b_f=2  acc_d=71  period=1333
+make ffk_POC_asic  addr=./paper_urgent/ffk_POC_asic_A_5_2_B_3_4_ACC_49_MHz750    a_e=5  a_f=2  b_e=3  b_f=4  acc_d=49  period=1333
+
+
+make ffk_POC_asic  addr=./paper_urgent/ffk_POC_asic_A_4_2_B_4_2_ACC_39_MHz750    a_e=4  a_f=2  b_e=4  b_f=2  acc_d=39  period=1333
+
+make ffk_POC_asic  addr=./paper_urgent/ffk_POC_asic_A_6_9_B_6_9_ACC_149_MHz750   a_e=6  a_f=9  b_e=6  b_f=9  acc_d=149 period=1333
+
+make ffk_POC_asic  addr=./paper_urgent/ffk_POC_asic_A_4_3_B_3_4_ACC_34_MHz750    a_e=4  a_f=3  b_e=3  b_f=4  acc_d=34  period=1333
+
+make ffk_POC_asic  addr=./paper_urgent/ffk_POC_asic_A_4_1_B_2_3_ACC_27_MHz750    a_e=4  a_f=1  b_e=2  b_f=3  acc_d=27  period=1333
+
+make ffk_POC_asic  addr=./paper_urgent/ffk_POC_asic_A_2_1_B_2_1_ACC_13_MHz750    a_e=2  a_f=1  b_e=2  b_f=1  acc_d=13  period=1333
+
