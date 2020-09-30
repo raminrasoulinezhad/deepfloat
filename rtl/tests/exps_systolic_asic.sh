bb4419f64ba074c0b7f5af8e67f3d6b303e02663
Seyedramin Rasoulinezhad <ramin_r348@yahoo.com>
2020-10-01 00:42:48 +1000

    update

--- rtl/tests/exps_systolic_asic.sh
+++ rtl/tests/exps_systolic_asic.sh
@@ -1,9 +1,27 @@
-make SystolicGrid_ffk_asic  addr=./ICLR/SystolicGrid_ffk_asic_A_2_5_B_2_5_ACC_31_tile_16_MHz750    a_e=2  a_f=5  b_e=2  b_f=5  acc_d=31  period=750  tile=16 
+make SystolicGrid_ffk_asic  addr=./ICLR/SystolicGrid_ffk_asic_A_2_5_B_2_5_ACC_31_tile_32_MHz750   a_e=2  a_f=5  b_e=2  b_f=5  acc_d=31  period=750  tile=32
+make SystolicGrid_ffk_asic  addr=./ICLR/SystolicGrid_ffk_asic_A_2_5_B_2_5_ACC_31_tile_16_MHz750   a_e=2  a_f=5  b_e=2  b_f=5  acc_d=31  period=750  tile=16 
 make SystolicGrid_ffk_asic  addr=./ICLR/SystolicGrid_ffk_asic_A_2_5_B_2_5_ACC_31_tile_8_MHz750    a_e=2  a_f=5  b_e=2  b_f=5  acc_d=31  period=750  tile=8 
-
-make SystolicGrid_ffk_asic  addr=./ICLR/SystolicGrid_ffk_asic_A_2_3_B_2_3_ACC_20_tile_16_MHz750    a_e=2  a_f=3  b_e=2  b_f=3  acc_d=20  period=750  tile=16 
+make SystolicGrid_ffk_asic  addr=./ICLR/SystolicGrid_ffk_asic_A_2_5_B_2_5_ACC_31_tile_4_MHz750    a_e=2  a_f=5  b_e=2  b_f=5  acc_d=31  period=750  tile=4 
+ 
+make SystolicGrid_ffk_asic  addr=./ICLR/SystolicGrid_ffk_asic_A_2_3_B_2_3_ACC_20_tile_32_MHz750   a_e=2  a_f=3  b_e=2  b_f=3  acc_d=20  period=750  tile=32 
+make SystolicGrid_ffk_asic  addr=./ICLR/SystolicGrid_ffk_asic_A_2_3_B_2_3_ACC_20_tile_16_MHz750   a_e=2  a_f=3  b_e=2  b_f=3  acc_d=20  period=750  tile=16 
 make SystolicGrid_ffk_asic  addr=./ICLR/SystolicGrid_ffk_asic_A_2_3_B_2_3_ACC_20_tile_8_MHz750    a_e=2  a_f=3  b_e=2  b_f=3  acc_d=20  period=750  tile=8 
+make SystolicGrid_ffk_asic  addr=./ICLR/SystolicGrid_ffk_asic_A_2_3_B_2_3_ACC_20_tile_4_MHz750    a_e=2  a_f=3  b_e=2  b_f=3  acc_d=20  period=750  tile=4 
 
+make SystolicGrid_ffk_asic  addr=./ICLR/SystolicGrid_ffk_asic_A_2_5_B_4_3_ACC_31_tile_32_MHz750   a_e=2  a_f=5  b_e=4  b_f=3  acc_d=31  period=750  tile=32
+make SystolicGrid_ffk_asic  addr=./ICLR/SystolicGrid_ffk_asic_A_2_5_B_4_3_ACC_31_tile_16_MHz750   a_e=2  a_f=5  b_e=4  b_f=3  acc_d=31  period=750  tile=16 
+make SystolicGrid_ffk_asic  addr=./ICLR/SystolicGrid_ffk_asic_A_2_5_B_4_3_ACC_31_tile_8_MHz750    a_e=2  a_f=5  b_e=4  b_f=3  acc_d=31  period=750  tile=8 
+make SystolicGrid_ffk_asic  addr=./ICLR/SystolicGrid_ffk_asic_A_2_5_B_4_3_ACC_31_tile_4_MHz750    a_e=2  a_f=5  b_e=4  b_f=3  acc_d=31  period=750  tile=4 
+
+make SystolicGrid_ffk_asic  addr=./ICLR/SystolicGrid_ffk_asic_A_2_3_B_3_2_ACC_20_tile_32_MHz750   a_e=2  a_f=3  b_e=3  b_f=2  acc_d=20  period=750  tile=32 
+make SystolicGrid_ffk_asic  addr=./ICLR/SystolicGrid_ffk_asic_A_2_3_B_3_2_ACC_20_tile_16_MHz750   a_e=2  a_f=3  b_e=3  b_f=2  acc_d=20  period=750  tile=16 
+make SystolicGrid_ffk_asic  addr=./ICLR/SystolicGrid_ffk_asic_A_2_3_B_3_2_ACC_20_tile_8_MHz750    a_e=2  a_f=3  b_e=3  b_f=2  acc_d=20  period=750  tile=8 
+make SystolicGrid_ffk_asic  addr=./ICLR/SystolicGrid_ffk_asic_A_2_3_B_3_2_ACC_20_tile_4_MHz750    a_e=2  a_f=3  b_e=3  b_f=2  acc_d=20  period=750  tile=4 
+
+make PaperIntegerSystolicGrid_asic  addr=./ICLR/PaperIntegerSystolicGrid_asic_A_B_8_ACC_32_tile_16_MHz750   a_b_s=8  acc_d=32 period=1333 tile=16
+make PaperIntegerSystolicGrid_asic  addr=./ICLR/PaperIntegerSystolicGrid_asic_A_B_8_ACC_32_tile_14_MHz750   a_b_s=8  acc_d=32 period=1333 tile=14
+make PaperIntegerSystolicGrid_asic  addr=./ICLR/PaperIntegerSystolicGrid_asic_A_B_8_ACC_32_tile_12_MHz750   a_b_s=8  acc_d=32 period=1333 tile=12
+make PaperIntegerSystolicGrid_asic  addr=./ICLR/PaperIntegerSystolicGrid_asic_A_B_8_ACC_32_tile_10_MHz750   a_b_s=8  acc_d=32 period=1333 tile=10
 make PaperIntegerSystolicGrid_asic  addr=./ICLR/PaperIntegerSystolicGrid_asic_A_B_8_ACC_32_tile_8_MHz750    a_b_s=8  acc_d=32 period=1333 tile=8
 
 
