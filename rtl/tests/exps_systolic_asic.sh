760b01ecec12b5f394c331b88ebe7faba749b894
Seyedramin Rasoulinezhad <ramin_r348@yahoo.com>
2020-09-30 20:46:38 +1000

    update

--- rtl/tests/exps_systolic_asic.sh
+++ rtl/tests/exps_systolic_asic.sh
@@ -1,2 +1,2 @@
-make SystolicGrid_ffk_asic  addr=./paper/SystolicGrid_ffk_asic_A_2_5_B_2_5_ACC_31_tile_32_MHz750    a_e=2  a_f=5  b_e=2  b_f=5  acc_d=31  period=750  tile=32 
-make SystolicGrid_ffk_asic  addr=./paper/SystolicGrid_ffk_asic_A_2_3_B_2_3_ACC_20_tile_32_MHz750    a_e=2  a_f=3  b_e=2  b_f=3  acc_d=20  period=750  tile=32 
+make SystolicGrid_ffk_asic  addr=./ICLR/SystolicGrid_ffk_asic_A_2_5_B_2_5_ACC_31_tile_32_MHz750    a_e=2  a_f=5  b_e=2  b_f=5  acc_d=31  period=750  tile=32 
+make SystolicGrid_ffk_asic  addr=./ICLR/SystolicGrid_ffk_asic_A_2_3_B_2_3_ACC_20_tile_32_MHz750    a_e=2  a_f=3  b_e=2  b_f=3  acc_d=20  period=750  tile=32 
