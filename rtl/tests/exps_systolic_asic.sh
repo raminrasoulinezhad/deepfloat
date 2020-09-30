d81959907d7bedda51d427cbabd16a9bb0af53fe
Seyedramin Rasoulinezhad <ramin_r348@yahoo.com>
2020-09-30 21:36:48 +1000

    results

--- rtl/tests/exps_systolic_asic.sh
+++ rtl/tests/exps_systolic_asic.sh
@@ -1,6 +1,7 @@
 make SystolicGrid_ffk_asic  addr=./ICLR/SystolicGrid_ffk_asic_A_2_5_B_2_5_ACC_31_tile_16_MHz750    a_e=2  a_f=5  b_e=2  b_f=5  acc_d=31  period=750  tile=16 
 make SystolicGrid_ffk_asic  addr=./ICLR/SystolicGrid_ffk_asic_A_2_3_B_2_3_ACC_20_tile_16_MHz750    a_e=2  a_f=3  b_e=2  b_f=3  acc_d=20  period=750  tile=16 
+make SystolicGrid_ffk_asic  addr=./ICLR/SystolicGrid_ffk_asic_A_2_3_B_2_3_ACC_20_tile_8_MHz750    a_e=2  a_f=3  b_e=2  b_f=3  acc_d=20  period=750  tile=8 
 
-make PaperIntegerSystolicGrid_asic  addr=./ICLR/PaperIntegerSystolicGrid_asic_A_B_8_ACC_32_tile_16_MHz750    a_b_s=8  acc_d=32 period=1333 tile=16
-make PaperIntegerSystolicGrid_asic  addr=./ICLR/PaperIntegerSystolicGrid_asic_A_B_8_ACC_32_tile_16_MHz750    a_b_s=8  acc_d=32 period=1333 tile=16
+make PaperIntegerSystolicGrid_asic  addr=./ICLR/PaperIntegerSystolicGrid_asic_A_B_8_ACC_32_tile_8_MHz750    a_b_s=8  acc_d=32 period=1333 tile=8
+
 
