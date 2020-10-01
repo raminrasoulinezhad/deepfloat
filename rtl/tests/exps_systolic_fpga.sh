e5c089a84ca76d2ed6aff7d191bef9eef6225584
Seyedramin Rasoulinezhad <ramin_r348@yahoo.com>
2020-10-01 18:05:07 +1000

    update

--- rtl/tests/exps_systolic_fpga.sh
+++ rtl/tests/exps_systolic_fpga.sh
@@ -1,3 +1,4 @@
+# 500 MHz
 make SystolicGrid_ffk_fpga  addr=./ICLR/SystolicGrid_ffk_fpga_A_2_5_B_2_5_ACC_31_tile_16_MHz500   a_e=2  a_f=5  b_e=2  b_f=5  acc_d=31  part=xcvu13p-fhga2104-1-i cons=Cons_500.xdc tile=16 &
 make SystolicGrid_ffk_fpga  addr=./ICLR/SystolicGrid_ffk_fpga_A_2_5_B_2_5_ACC_31_tile_8_MHz500    a_e=2  a_f=5  b_e=2  b_f=5  acc_d=31  part=xcvu13p-fhga2104-1-i cons=Cons_500.xdc tile=8 &
 make SystolicGrid_ffk_fpga  addr=./ICLR/SystolicGrid_ffk_fpga_A_2_5_B_2_5_ACC_31_tile_4_MHz500    a_e=2  a_f=5  b_e=2  b_f=5  acc_d=31  part=xcvu13p-fhga2104-1-i cons=Cons_500.xdc tile=4 &
@@ -24,3 +25,33 @@ make PaperIntegerSystolicGrid_fpga  addr=./ICLR/PaperIntegerSystolicGrid_fpga_A_
 make SystolicGrid_fff_fpga  addr=./ICLR/SystolicGrid_fff_fpga_A_4_3_B_5_2_ACC_6_9_tile_16_MHz500  a_e=4  a_f=3  b_e=5  b_f=2  acc_e=6   acc_f=9    part=xcvu13p-fhga2104-1-i cons=Cons_500.xdc tile=16 &
 make SystolicGrid_fff_fpga  addr=./ICLR/SystolicGrid_fff_fpga_A_4_3_B_5_2_ACC_6_9_tile_8_MHz500   a_e=4  a_f=3  b_e=5  b_f=2  acc_e=6   acc_f=9    part=xcvu13p-fhga2104-1-i cons=Cons_500.xdc tile=8 &
 make SystolicGrid_fff_fpga  addr=./ICLR/SystolicGrid_fff_fpga_A_4_3_B_5_2_ACC_6_9_tile_4_MHz500   a_e=4  a_f=3  b_e=5  b_f=2  acc_e=6   acc_f=9    part=xcvu13p-fhga2104-1-i cons=Cons_500.xdc tile=4 &
+
+
+
+# 250 MHz
+make SystolicGrid_ffk_fpga  addr=./ICLR/SystolicGrid_ffk_fpga_A_2_5_B_2_5_ACC_31_tile_16_MHz250   a_e=2  a_f=5  b_e=2  b_f=5  acc_d=31  part=xcvu13p-fhga2104-1-i cons=Cons_250.xdc tile=16 &
+make SystolicGrid_ffk_fpga  addr=./ICLR/SystolicGrid_ffk_fpga_A_2_5_B_2_5_ACC_31_tile_8_MHz250    a_e=2  a_f=5  b_e=2  b_f=5  acc_d=31  part=xcvu13p-fhga2104-1-i cons=Cons_250.xdc tile=8 &
+make SystolicGrid_ffk_fpga  addr=./ICLR/SystolicGrid_ffk_fpga_A_2_5_B_2_5_ACC_31_tile_4_MHz250    a_e=2  a_f=5  b_e=2  b_f=5  acc_d=31  part=xcvu13p-fhga2104-1-i cons=Cons_250.xdc tile=4 &
+
+make SystolicGrid_ffk_fpga  addr=./ICLR/SystolicGrid_ffk_fpga_A_2_3_B_2_3_ACC_20_tile_16_MHz250   a_e=2  a_f=3  b_e=2  b_f=3  acc_d=20  part=xcvu13p-fhga2104-1-i cons=Cons_250.xdc tile=16 &
+make SystolicGrid_ffk_fpga  addr=./ICLR/SystolicGrid_ffk_fpga_A_2_3_B_2_3_ACC_20_tile_8_MHz250    a_e=2  a_f=3  b_e=2  b_f=3  acc_d=20  part=xcvu13p-fhga2104-1-i cons=Cons_250.xdc tile=8 &
+make SystolicGrid_ffk_fpga  addr=./ICLR/SystolicGrid_ffk_fpga_A_2_3_B_2_3_ACC_20_tile_4_MHz250    a_e=2  a_f=3  b_e=2  b_f=3  acc_d=20  part=xcvu13p-fhga2104-1-i cons=Cons_250.xdc tile=4 &
+
+
+make SystolicGrid_ffk_fpga  addr=./ICLR/SystolicGrid_ffk_fpga_A_2_5_B_4_3_ACC_31_tile_16_MHz250   a_e=2  a_f=5  b_e=4  b_f=3  acc_d=31  part=xcvu13p-fhga2104-1-i cons=Cons_250.xdc tile=16 &
+make SystolicGrid_ffk_fpga  addr=./ICLR/SystolicGrid_ffk_fpga_A_2_5_B_4_3_ACC_31_tile_8_MHz250    a_e=2  a_f=5  b_e=4  b_f=3  acc_d=31  part=xcvu13p-fhga2104-1-i cons=Cons_250.xdc tile=8 &
+make SystolicGrid_ffk_fpga  addr=./ICLR/SystolicGrid_ffk_fpga_A_2_5_B_4_3_ACC_31_tile_4_MHz250    a_e=2  a_f=5  b_e=4  b_f=3  acc_d=31  part=xcvu13p-fhga2104-1-i cons=Cons_250.xdc tile=4 &
+
+make SystolicGrid_ffk_fpga  addr=./ICLR/SystolicGrid_ffk_fpga_A_2_3_B_3_2_ACC_20_tile_16_MHz250   a_e=2  a_f=3  b_e=3  b_f=2  acc_d=20  part=xcvu13p-fhga2104-1-i cons=Cons_250.xdc tile=16 &
+make SystolicGrid_ffk_fpga  addr=./ICLR/SystolicGrid_ffk_fpga_A_2_3_B_3_2_ACC_20_tile_8_MHz250    a_e=2  a_f=3  b_e=3  b_f=2  acc_d=20  part=xcvu13p-fhga2104-1-i cons=Cons_250.xdc tile=8 &
+make SystolicGrid_ffk_fpga  addr=./ICLR/SystolicGrid_ffk_fpga_A_2_3_B_3_2_ACC_20_tile_4_MHz250    a_e=2  a_f=3  b_e=3  b_f=2  acc_d=20  part=xcvu13p-fhga2104-1-i cons=Cons_250.xdc tile=4 &
+
+
+make PaperIntegerSystolicGrid_fpga  addr=./ICLR/PaperIntegerSystolicGrid_fpga_A_B_8_ACC_32_tile_16_MHz250  a_b_s=8  acc_d=32  part=xcvu13p-fhga2104-1-i cons=Cons_250.xdc tile=16 &
+make PaperIntegerSystolicGrid_fpga  addr=./ICLR/PaperIntegerSystolicGrid_fpga_A_B_8_ACC_32_tile_8_MHz250   a_b_s=8  acc_d=32  part=xcvu13p-fhga2104-1-i cons=Cons_250.xdc tile=8 &
+make PaperIntegerSystolicGrid_fpga  addr=./ICLR/PaperIntegerSystolicGrid_fpga_A_B_8_ACC_32_tile_4_MHz250   a_b_s=8  acc_d=32  part=xcvu13p-fhga2104-1-i cons=Cons_250.xdc tile=4 &
+
+
+make SystolicGrid_fff_fpga  addr=./ICLR/SystolicGrid_fff_fpga_A_4_3_B_5_2_ACC_6_9_tile_16_MHz250  a_e=4  a_f=3  b_e=5  b_f=2  acc_e=6   acc_f=9    part=xcvu13p-fhga2104-1-i cons=Cons_250.xdc tile=16 &
+make SystolicGrid_fff_fpga  addr=./ICLR/SystolicGrid_fff_fpga_A_4_3_B_5_2_ACC_6_9_tile_8_MHz250   a_e=4  a_f=3  b_e=5  b_f=2  acc_e=6   acc_f=9    part=xcvu13p-fhga2104-1-i cons=Cons_250.xdc tile=8 &
+make SystolicGrid_fff_fpga  addr=./ICLR/SystolicGrid_fff_fpga_A_4_3_B_5_2_ACC_6_9_tile_4_MHz250   a_e=4  a_f=3  b_e=5  b_f=2  acc_e=6   acc_f=9    part=xcvu13p-fhga2104-1-i cons=Cons_250.xdc tile=4 &
